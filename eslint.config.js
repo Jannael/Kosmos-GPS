b5ecf81ba94d934d9cfb536868d253e481d2e377
Jannael <orlandojannael@gmail.com>
2026-06-20 16:11:49 -0600

    chore: add eslint, prettier, and lint-staged configuration

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js'
+import typescriptEslint from 'typescript-eslint'
+
+import eslintPluginAstro from 'eslint-plugin-astro'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+
+export default [
+	{ ignores: ['**/dist/**', '**/node_modules/**', '**/build/**', '', '**/.astro/**'] },
+	js.configs.recommended,
+	...typescriptEslint.configs.recommended,
+
+	...eslintPluginAstro.configs.recommended,
+	{
+		files: ['**/*.tsx', '**/*.jsx'],
+		plugins: { react, 'react-hooks': reactHooks },
+		languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } },
+		settings: { react: { version: '19.0' } },
+	},
+	{ files: ['*.ts', '*.tsx', '*.astro', '*.js', '*.jsx'] },
+]
